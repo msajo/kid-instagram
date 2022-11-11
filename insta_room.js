066d05474368492b35c1f10beffb637277d5c7a3
COOL- <87934102+msajo@users.noreply.github.com>
2022-11-11 16:15:03 +1100

    Add files via upload

--- insta_room.js
+++ insta_room.js
@@ -20,3 +20,17 @@ var firebaseConfig = {
        });});}
  getData();
   
+ function  addRoom(){
+  room_name = document.getElementById("room_name").value;
+  firebase.database().ref("/").child("room_name",room_name).update({
+    purpose : "adding room name"
+  });
+  localStorage.setItem("room_name",room_name);
+  window.location = "Insta_page.html";
+ }
+
+ function redirectToRoomName(name){
+   console.log(name);
+   localStorage.setItem("room_name",name);
+   window.location = "Insta_page.html";
+ }
